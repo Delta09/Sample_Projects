30a19e0b8cd2088686b3d476c4eaa5e7762c79dd
Daanyaal Saeed <daanyaalsaeed@live.com>
2021-10-16 14:16:26 -0700

    update 10/16

---
+++ Learning/complete-node-bootcamp/4-natours/starter/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) MIDDLEWARES
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// app.use(express.static(`${__dirname}/public`));
+
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
---
+++ Learning/complete-node-bootcamp/4-natours/starter/controllers/userController.js
@@ -0,0 +1,33 @@
+exports.getAllUsers = (req, res) => {
+  return res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+exports.getUser = (req, res) => {
+  return res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+exports.createUser = (req, res) => {
+  return res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  return res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+exports.deleteUser = (req, res) => {
+  return res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
